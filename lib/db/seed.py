d4a283dc8fbb4341f50346301035236f28baa164
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-27 20:43:33 -0700

    add generate book data for db

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -15,19 +15,34 @@ fake = Faker()
 
 book_store_names = ["Book Addict", "Book Bin", "Hooked on Books", "The Book Worm", "A Likely Story", "Books 4 Less", "Open Books", "Neighborhood Books", "Booked", "The Book Store"]
 
+book_genres = ["Mystery", "Fiction", "Non-fiction", "Science fiction", "Thriller", "Horror", "Graphic novel", "Poetry", "Memoir", "Children's literature", "Biography", "Mythology", "Young adult", "Fantasy", "Fairy tale"]
+
 if __name__ == '__main__':
     # Delete Table Data
     session.query(Store).delete()
-
+    session.query(Book).delete()
 
     book_stores = []
     for store in book_store_names:
         b_store = Store(
             store_name = store,
             address = fake.address(),
-            phone = random.randint(1000000000, 9999999999)
+            phone = fake.phone_number()
         )
         
         session.add(b_store)
         session.commit()
         book_stores.append(b_store)
+
+    all_books = []
+    for _ in range (100):
+        book = Book(
+            title = fake.word(),
+            author = f"{fake.first_name()} {fake.last_name()} ",
+            genre = random.choice(book_genres),
+            price = "%.2f" % random.uniform(9.50, 40.00)
+        )
+        
+        session.add(book)
+        session.commit()
+        all_books.append(book)
