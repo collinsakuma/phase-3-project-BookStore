2ea28b61e74ca27559fd1c5ddee8e8c259e92927
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-29 13:43:18 -0700

    Delete Cart at session start

--- lib/cli.py
+++ lib/cli.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db.models import Base, Store, Book, Inventory
-from helpers import create_stores_table, cli_start
+from db.models import Base, Store, Book, Inventory, Cart
+from helpers import create_stores_table, cli_start, session
 
-engine = create_engine('sqlite:///db/book_stores.db')
-Base.metadata.create_all(engine)
+# engine = create_engine('sqlite:///db/book_stores.db')
+# Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
 if __name__ == '__main__':
+    session.query(Cart).delete()
     print('Welcome to our Book store CLI app!')
-
     cli_start()
